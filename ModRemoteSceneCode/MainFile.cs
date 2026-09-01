b8d75f59cd9144890e918f79bfdb08ea5eb74b82
quipsol <245758987+quipsol@users.noreply.github.com>
2026-09-01 17:15:39 +0200

    Update MainFile
    The scripts already exist. Looking them up again would throw an error. If we build for the editor, don't include the Bridge line.

--- ModRemoteSceneCode/MainFile.cs
+++ ModRemoteSceneCode/MainFile.cs
@@ -8,17 +8,19 @@ namespace ModRemoteScene.ModRemoteSceneCode;
 [ModInitializer(nameof(Initialize))]
 public partial class MainFile : Node
 {
-    public const string ModId = "ModRemoteScene"; //At the moment, this is used only for the Logger and harmony names.
+    public const string MOD_ID = "ModRemoteScene"; //At the moment, this is used only for the Logger and harmony names.
 
     public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } = new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
 
     public static void Initialize()
     {
-        //If you want to use scripts defined in your mod for Godot scenes, uncomment the following line.
-        //Godot.Bridge.ScriptManagerBridge.LookupScriptsInAssembly(Assembly.GetExecutingAssembly());
-
+        // TOOLS is defined when building with the Debug configuration (editor and editor player)
+        // https://docs.godotengine.org/en/4.4/tutorials/scripting/c_sharp/c_sharp_features.html
+#if !TOOLS
+        Godot.Bridge.ScriptManagerBridge.LookupScriptsInAssembly(Assembly.GetExecutingAssembly());
+#endif
+        
         Harmony harmony = new(ModId);
-
         harmony.PatchAll();
     }
 }

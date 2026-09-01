8d9618036b54bfd604dee56eb5e42ec1bd2f3694
quipsol <245758987+quipsol@users.noreply.github.com>
2026-09-01 17:26:54 +0200

    Irrelevant

---
+++ ModRemoteSceneCode/Examples/AddModImageToAllCards.cs
@@ -0,0 +1,10 @@
+﻿namespace ModRemoteScene.ModRemoteSceneCode.Examples;
+
+
+/// <summary>
+/// AddedNode example to show modded content within the context of the game scenes via remote scene tree.
+/// </summary>
+public class AddModImageToAllCards
+{
+    
+}
---
+++ ModRemoteSceneCode/Examples/MainMenuLogMessage.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Screens.MainMenu;
+
+namespace ModRemoteScene.ModRemoteSceneCode.Examples;
+
+/// <summary>
+/// Example of logs showing in the Editor.
+/// </summary>
+[HarmonyPatch(typeof(NMainMenu), nameof(NMainMenu._Ready))]
+public static class MainMenuLogMessage
+{
+    [HarmonyPrefix]
+    public static void Log()
+    {
+        GD.Print("This message will appear in the logs!");
+    }
+}
